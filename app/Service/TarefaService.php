64aadb0fba5826fece1cf9f12dd7b2e1088c8b32
joaogabrielfossadeoliveira <joaogabrielfo24@gmail.com>
2024-09-13 10:55:56 -0300

    first commit

---
+++ app/Service/TarefaService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Tarefa;
+
+class TarefaService{
+public function create(array $tarefa){
+    $dados = Tarefa::create([
+        'titulo' => $tarefa['titulo'],
+        'descricao' => $tarefa['descricao'],
+        'status' => 'Esta em aberto'
+        
+
+]);
+    return $dados;
+
+}
+public function findById($id)
+{
+ $tarefa = Tarefa::find($id);
+ if($tarefa == null){
+    return[
+        'status'=>false,
+        'message'=>'Tarefa não encontrada'
+    ];
+ }
+ return [
+    'status'=>true,
+    'message'=>'Pesquisa realizada com sucesso',
+    'data'=> $tarefa
+ ];
+}
+public function getAll()
+{
+    $tarefas = Tarefa::all();
+    return[
+        'status'=>true,
+    'message'=>'Pesquisa efetuada com sucesso',
+    'data'=> $tarefas
+    ];
+}
+};
+
+
